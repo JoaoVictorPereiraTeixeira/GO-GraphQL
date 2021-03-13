16eb713a8d24ef9f3ce556113a07ce16d45359ec
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-03-13 13:43:03 -0300

    Finished project

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,13 @@
 package graph
 
+import "github.com/JoaoVictorTeixeira9819/graphql-go/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
+	Categories []*model.Category
+	Courses    []*model.Course
+	Chapters   []*model.Chapter
 }
